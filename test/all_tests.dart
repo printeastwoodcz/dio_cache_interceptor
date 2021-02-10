30bc60798f6257134f10c11799a0dce8fa461bd4
Lukas Svoboda <printeastwoodcz@gmail.com>
2021-02-10 11:09:04 +0100

    tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -2,9 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'file_cache_store.dart' as file_store_test;
 import 'mem_cache_store.dart' as mem_store_test;
+import 'models.dart' as models_test;
 
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   file_store_test.main();
   mem_store_test.main();
+  models_test.main();
 }
